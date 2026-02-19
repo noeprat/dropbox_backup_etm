bfa8be8af440f968a7ed5e5ff6d8129e8af713a9
Noe Prat <117754404+noeprat@users.noreply.github.com>
2026-02-19 16:45:38 +0100

    minor fix if is_derivative is precised

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -25,9 +25,8 @@ def handle_exceptions(exceptions_path, file_infos_path, new_file_infos_path, deb
                     new_file_infos[file][key] = exceptions[except_string][key]
             if "type" in exceptions[except_string].keys():
                 new_type = new_file_infos[file]["type"]
-                new_file_infos[file]["is_derivative"] = is_derivative(new_type)
+                if "is_derivative" not in exceptions[except_string].keys():
+                    new_file_infos[file]["is_derivative"] = is_derivative(new_type)
     
     with open(new_file_infos_path, "w") as f3:
         json.dump(new_file_infos, f3,  indent=4)
-
-        
